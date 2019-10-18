932c677872a03c9dd8b733746f13cda2fc52601e
Burak Yılmaz <byilmaz50@hotmail.com>
2019-10-18 18:26:41 +0300

    Kullanıcı Giriş
    +Kullanıcı giriş ve üye olma ekranı uyarıları Tamamlandı.

--- epubDeneme/epubDeneme/UyeGirisiVC.swift
+++ epubDeneme/epubDeneme/UyeGirisiVC.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class UyeGirisiVC: UIViewController ,UITextFieldDelegate{
-
+    
     @IBOutlet weak var epostaAdresi: UITextField!
     @IBOutlet weak var parola: UITextField!
     @IBOutlet weak var oturumAc: UIButton!
@@ -20,6 +20,7 @@ class UyeGirisiVC: UIViewController ,UITextFieldDelegate{
     override func viewDidLoad() {
         super.viewDidLoad()
         epostaAdresi.delegate = self
+        parola.delegate = self
         
         parola.layer.cornerRadius = 15
         parola.layer.borderColor = UIColor.lightGray.cgColor
@@ -28,7 +29,7 @@ class UyeGirisiVC: UIViewController ,UITextFieldDelegate{
         epostaAdresi.layer.cornerRadius = 15
         epostaAdresi.layer.borderColor = UIColor.lightGray.cgColor
         epostaAdresi.layer.borderWidth = 1.0
-
+        
         oturumAc.layer.cornerRadius = 15
         
         let emailImage = UIImage(named: "mail")
@@ -36,7 +37,7 @@ class UyeGirisiVC: UIViewController ,UITextFieldDelegate{
         
         let passwordImage = UIImage(named : "password")
         addLeftImage(txtField: parola, andImage: passwordImage!)
-
+        
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
--- epubDeneme/epubDeneme/UyeOlVC.swift
+++ epubDeneme/epubDeneme/UyeOlVC.swift
@@ -28,69 +28,147 @@ let formMessage : String?
 let exception : String?
 }
 
-class UyeOlVC: UIViewController {
+class UyeOlVC: UIViewController , UITextFieldDelegate{
 
     @IBOutlet weak var isimSoyisim: UITextField!
     @IBOutlet weak var mailAdresi: UITextField!
     @IBOutlet weak var parola: UITextField!
     @IBOutlet weak var parolaTekrari: UITextField!
+    @IBOutlet weak var uyeOl: UIButton!
     
     @IBAction func UyeOl(_ sender: Any) {
-        let parameters = ["email": mailAdresi.text as Any, "sifre": parola.text as Any] as [String : Any]
+        
+        if(isimSoyisim.text!.count < 4){
+            let alert = UIAlertController(title: "Uyarı", message: "İsim soyisim alanı boş bırakılmamalıdır.", preferredStyle: .alert)
+            let okButton = UIAlertAction(title: "Tamam", style: .cancel, handler: nil)
+            alert.addAction(okButton)
+            self.present(alert, animated: true, completion: nil)
+        }
+        else if(mailAdresi.text!.count < 1){
+            let alert = UIAlertController(title: "Uyarı", message: "Mail adresi giriniz.", preferredStyle: .alert)
+            let okButton = UIAlertAction(title: "Tamam", style: .cancel, handler: nil)
+            alert.addAction(okButton)
+            self.present(alert, animated: true, completion: nil)
+        }
+            
+        else if(mailAdresi.text!.count > 1){
+            let isEmailAddressValid =  isValidEmail(testStr: mailAdresi.text!)
+            if ( !isEmailAddressValid ){
+                let alert = UIAlertController(title: "Mail Adresi", message: "Else Geçersiniz mail", preferredStyle: .alert)
+                let okButton = UIAlertAction(title: "Tamam", style: .cancel, handler: nil)
+                alert.addAction(okButton)
+                self.present(alert, animated: true, completion: nil)
+            }
+            else{
+                if(parola.text!.count < 6){
+                    let alert = UIAlertController(title: "Parola", message: "Parola en az 6 karakter içermelidir.", preferredStyle: .alert)
+                    let okButton = UIAlertAction(title: "Tamam", style: .cancel, handler: nil)
+                    alert.addAction(okButton)
+                    self.present(alert, animated: true, completion: nil)
+                }
+                    
+                else if(parola.text != parolaTekrari.text){
+                    let alert = UIAlertController(title: "Parola", message: "Parolalar uyuşmamaktadır.", preferredStyle: .alert)
+                    let okButton = UIAlertAction(title: "Tamam", style: .cancel, handler: nil)
+                    alert.addAction(okButton)
+                    self.present(alert, animated: true, completion: nil)
+                }
+                        else{
+                    let parameters = ["email": mailAdresi.text as Any, "sifre": parola.text as Any] as [String : Any]
 
-           //create the url with URL
-           let url = URL(string: "http://e-kitaplik.net/api/kullanici")! //change the url
+                       //create the url with URL
+                       let url = URL(string: "http://e-kitaplik.net/api/kullanici")! //change the url
 
-           //create the session object
-           let session = URLSession.shared
+                       //create the session object
+                       let session = URLSession.shared
 
-           //now create the URLRequest object using the url object
-           var request = URLRequest(url: url)
-           request.httpMethod = "POST" //set http method as POST
+                       //now create the URLRequest object using the url object
+                       var request = URLRequest(url: url)
+                       request.httpMethod = "POST" //set http method as POST
 
-           do {
-               request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
-           } catch let error {
-               print(error.localizedDescription)
-           }
+                       do {
+                           request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
+                       } catch let error {
+                           print(error.localizedDescription)
+                       }
 
-           request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-           request.addValue("application/json", forHTTPHeaderField: "Accept")
+                       request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+                       request.addValue("application/json", forHTTPHeaderField: "Accept")
 
-           //create dataTask using the session object to send data to the server
-           let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+                       //create dataTask using the session object to send data to the server
+                       let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
 
-               guard error == nil else {
-                   return
-               }
+                           guard error == nil else {
+                               return
+                           }
 
-               guard let data = data else {
-                   return
-               }
+                           guard let data = data else {
+                               return
+                           }
 
-               do {
-                   //create json object from data
-                   if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
-                       print(json)
-                       // handle json...
-                    let decoder = JSONDecoder()
-                    let gitData = try decoder.decode(myData.self, from: data)
-                    if(gitData.isSuccess! == true){
-                      DispatchQueue.main.async(){
-                           self.performSegue(withIdentifier: "uyeOldun", sender: self)
-                        }
+                           do {
+                               //create json object from data
+                               if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                                   print(json)
+                                   // handle json...
+                                let decoder = JSONDecoder()
+                                let gitData = try decoder.decode(myData.self, from: data)
+                                if(gitData.isSuccess! == true){
+                                  DispatchQueue.main.async(){
+                                       self.performSegue(withIdentifier: "uyeOldun", sender: self)
+                                    }
+                                }
+                               }
+                           } catch let error {
+                               print(error.localizedDescription)
+                           }
+                       })
+                       task.resume()
                     }
-                   }
-               } catch let error {
-                   print(error.localizedDescription)
-               }
-           })
-           task.resume()
+                }
+            }
+            
+        }
+
+        
+
+    func isValidEmail(testStr:String) -> Bool {
+         let emailRegEx = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{1,4}$"
+         let emailTest = NSPredicate(format:"SELF MATCHES[c] %@", emailRegEx)
+         return emailTest.evaluate(with: testStr)
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        
+        isimSoyisim.delegate = self
+        mailAdresi.delegate = self
+        parola.delegate = self
+        parolaTekrari.delegate = self
+        
+        isimSoyisim.layer.cornerRadius = 15
+        mailAdresi.layer.cornerRadius = 15
+        parola.layer.cornerRadius = 15
+        parolaTekrari.layer.cornerRadius = 15
+        uyeOl.layer.cornerRadius = 15
+        
+        isimSoyisim.layer.sublayerTransform = CATransform3DMakeTranslation(5, 0, 10)
+        mailAdresi.layer.sublayerTransform = CATransform3DMakeTranslation(5, 0, 10)
+        parola.layer.sublayerTransform = CATransform3DMakeTranslation(5, 0, 10)
+        parolaTekrari.layer.sublayerTransform = CATransform3DMakeTranslation(5, 0, 10)
+        uyeOl.layer.sublayerTransform = CATransform3DMakeTranslation(5, 0, 10)
+        
 
+        
     }
     
 }
